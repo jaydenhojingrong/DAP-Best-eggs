7e10e61ca33eb01811e65a94ef62de001b6649a0
FionaWee <54692252+FionaWee@users.noreply.github.com>
2020-01-23 00:34:31 +0800

    summary
    edited

--- GetOldTweets-python-master/Main.py
+++ GetOldTweets-python-master/Main.py
@@ -1,13 +1,56 @@
+
+
+
+
 import sys
 if sys.version_info[0] < 3:
     import got
 else:
 	import got3 as got
 
+import csv
+
 def main():
 
-	def printTweet(descr, t):
-		print(f'{descr}: {t.text} {t.date}')
+	def printTweet(t):
+		return([t.text, t.date])
+		# print(descr)
+		# print("Username: %s" % t.username)
+		# print("Retweets: %d" % t.retweets)
+		# print("Text: %s" % t.text)
+		# print("Mentions: %s" % t.mentions)
+		# print("Hashtags: %s\n" % t.hashtags)
+
+	# Example 2 - Get tweets by query search
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('trump').setSince("2016-06-16").setNear("America").setUntil("2016-09-30").setMaxTweets(100)
+	#print(len(got.manager.TweetManager.getTweets(tweetCriteria)))
+
+	with open('data.csv', mode='w') as employee_file:
+		for i in range(10):
+			tweet = got.manager.TweetManager.getTweets(tweetCriteria)[i]
+			employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+			employee_writer.writerow(printTweet(tweet))	
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+# import sys
+# if sys.version_info[0] < 3:
+#     import got
+# else:
+# 	import got3 as got
+
+# def main():
+
+# 	def printTweet(descr, t):
+# 		print(f'{descr}: {t.text} {t.date}')
 		# print(descr)
 		# print("Username: %s" % t.username)
 		# print("Retweets: %d" % t.retweets)
@@ -22,12 +65,12 @@ def main():
 	# printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
 
 	# Example 2 - Get tweets by query search
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('trump').setSince("2016-06-16").setNear("America").setUntil("2016-09-30").setMaxTweets(100)
+	# tweetCriteria = got.manager.TweetCriteria().setQuerySearch('trump').setSince("2016-06-16").setNear("America").setUntil("2016-09-30").setMaxTweets(100)
 	#print(len(got.manager.TweetManager.getTweets(tweetCriteria)))
-	for i in range(10):
-		tweet = got.manager.TweetManager.getTweets(tweetCriteria)[i]
+	# for i in range(10):
+	# 	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[i]
 
-		printTweet(i, tweet)
+	# 	printTweet("### Example 2 - Get tweets including trump", tweet)
 
 	# # Example 3 - Get tweets by username and bound dates
 	# tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
@@ -35,5 +78,5 @@ def main():
 
 	# printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()
