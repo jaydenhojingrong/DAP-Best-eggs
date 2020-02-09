03d94d199e8b497a86dbcc406fee1c4cf92daf41
jaydenhojingrong <jinglong05@hotmail.com>
2020-02-09 17:39:14 +0800

    cleaning alllll
    santistized egg

--- GetOldTweets-python-master/Cleaned/cleaning.py
+++ GetOldTweets-python-master/Cleaned/cleaning.py
@@ -4,18 +4,18 @@ import csv
 import os
 
 tweets = list()
-directory = "C:\\Users\\jingl\\OneDrive\\Documents\\GitHub\\DAP-Best-eggs\\GetOldTweets-python-master\\Exporter csv\\Trump"
+directory = "C:\\Users\\jingl\\OneDrive\\Documents\\GitHub\\DAP-Best-eggs\\GetOldTweets-python-master\\Exporter csv\\Hillary"
 for filename in os.listdir(directory):
     if filename.endswith(".csv"): 
         print(filename)
         
-        with open(filename, encoding='utf8', errors="ignore") as csvfile:
+        with open(directory + "\\" + filename, encoding='utf8', errors="ignore") as csvfile:
             next(csvfile)
             for row in csvfile:
                 row = row.strip().split(";")
                 tweets.append([row[1],p.clean(row[4].strip("\""))])
 
-with open('trump.csv', 'a', newline='', errors="ignore") as csvfile:
+with open('hillary.csv', 'a', newline='', errors="ignore") as csvfile:
     writer = csv.writer(csvfile)
     for tweet in tweets:
         writer.writerow(tweet)
