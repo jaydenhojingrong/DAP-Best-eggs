55d3bffa75d4aa8930dbb4efa42dd981ec55a46c
jaydenhojingrong <jinglong05@hotmail.com>
2020-02-16 16:26:25 +0800

    with comments
    egg

--- GetOldTweets-python-master/Cleaned/cleaning.py
+++ GetOldTweets-python-master/Cleaned/cleaning.py
@@ -4,6 +4,12 @@ import csv
 import os
 import sys
 
+'''
+Output format is:
+{username1: (date1, [sentence1, sentence2]), date2, [sentence1, sentence2], username2: ....}
+    ^key     ^ tuple with date and tweet sentences          ^ tweet sentences is a list
+'''
+
 tweets = dict()
 user = str()
 pre_clean_tweet = str()
@@ -23,36 +29,44 @@ else:
 
 directory = "C:\\Users\\jingl\\OneDrive\\Documents\\GitHub\\DAP-Best-eggs\\GetOldTweets-python-master\\Exporter csv\\" + keyword
 
+#loop thru all csv in the above directory
 for filename in os.listdir(directory):
     if filename.endswith(".csv"): 
         print(filename)
         
         with open(directory + "\\" + filename, encoding='utf8', errors="ignore") as csvfile:
+            #skip column headers
             next(csvfile)
             for row in csvfile:
+                #in clinton file, we ignore tweets with hillary keyword inside
+                #to avoid duplicates
                 if (keyword == "Clinton"):
                     if ("hillary" in row.lower()):
                         continue
-
+                #data delimiter is seperated by ;
                 row = row.strip().split(";")
 
+                #split URL to extract username
                 user = row[-1].split("/")
                 user = user[3]
 
+                #split actual tweet by sentence and clean each sentence
                 pre_clean_tweet = TextBlob(row[4].strip("\""))
                 pre_clean_tweet = pre_clean_tweet.sentences
 
                 for sentence in pre_clean_tweet:
                     sentence = p.clean(str(sentence))
                     cleaned_tweet.append(sentence)
-
+                
+                #populate dictionary
                 if (user in tweets):
                     tweets[user].append(tuple((row[1], cleaned_tweet)))
                 else:
                     tweets[user] = [tuple((row[1], cleaned_tweet))]
-
+                #reset clean_tweet
                 cleaned_tweet = list()
 
+#populate csv
 with open(keyword + '.csv', 'a', newline='', errors="ignore") as csvfile:
     writer = csv.DictWriter(csvfile, ["user", "tweets"])
     for user, date_tweet in tweets.items():
