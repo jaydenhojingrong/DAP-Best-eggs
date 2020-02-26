c1727a65c105d82d495f3403d5e0ae23363af5a6
jaydenhojingrong <jinglong05@hotmail.com>
2020-02-27 01:23:25 +0800

    textblooob
    test

---
+++ GetOldTweets-python-master/Cleaned/cleaning - Copy.py
@@ -0,0 +1,76 @@
+import preprocessor as p
+from textblob import TextBlob
+import csv
+import os
+import sys
+
+'''
+Output format is:
+{username1: (date1, [sentence1, sentence2]), date2, [sentence1, sentence2], username2: ....}
+    ^key     ^ tuple with date and tweet sentences          ^ tweet sentences is a list
+'''
+
+tweets = list()
+user = str()
+pre_clean_tweet = str()
+cleaned_tweet = list()
+
+#keyword = input("Enter: \n [1] for trump \n [2] for hillary \n [3] for clinton \n")
+keyword = "3"
+if (keyword == "1"):
+    keyword = "Trump"
+elif (keyword == "2"):
+    keyword = "Hillary"
+elif (keyword == "3"):
+    keyword = "Clinton"
+else:
+    print("errrr......")
+    sys.exit()
+
+directory = "C:\\Users\\jingl\\OneDrive\\Documents\\GitHub\\DAP-Best-eggs\\GetOldTweets-python-master\\Exporter csv\\" + keyword
+
+#loop thru all csv in the above directory
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"): 
+        print(filename)
+        
+        with open(directory + "\\" + filename, encoding='utf8', errors="ignore") as csvfile:
+            #skip column headers
+            next(csvfile)
+            for row in csvfile:
+                #in clinton file, we ignore tweets with hillary keyword inside
+                #to avoid duplicates
+                if (keyword == "Clinton"):
+                    if ("hillary" in row.lower()):
+                        continue
+                #data delimiter is seperated by ;
+                row = row.strip().split(";")
+
+                #split URL to extract username
+                user = row[-1].split("/")
+                user = user[3]
+
+                #split actual tweet by sentence and clean each sentence
+                pre_clean_tweet = TextBlob(row[4].strip("\""))
+                pre_clean_tweet = pre_clean_tweet.sentences
+
+                for sentence in pre_clean_tweet:
+                    sentence = p.clean(str(sentence))
+                    if len(sentence) > 3: 
+                        tweets.append(list((user, row[1], sentence)))
+
+                #reset clean_tweet
+                cleaned_tweet = list()
+
+#populate csv
+with open(keyword + '2.csv', 'a', newline='', errors="ignore") as csvfile:
+    writer = csv.writer(csvfile)
+    for tweet in tweets:
+        writer.writerow([tweet[0], tweet[1], tweet[2]])
+
+
+
+
+
+
+
